57e8a8a44b2ec00c4fdd21aae2e1b7e2d68a8bf0
Marc Widmer <widmer@ubique.ch>
2022-11-04 11:49:40 +0100

    wip - back to ISL for a bit

---
+++ acme/challenge.go
@@ -0,0 +1,25 @@
+package main
+
+type challenge struct {
+	challengeType string `json:"type"`
+	url           string `json:"url"`
+	token         string `json:"token"`
+}
+
+type dnsRegistration struct {
+}
+
+func (acme *acmeConfig) registerDNSChallenge(chal *challenge) (*dnsRegistration, error) {
+	// TODO
+	return nil, nil
+}
+
+func (acme *acmeConfig) deregisterDNSChallenge(reg *dnsRegistration) error {
+	// TODO
+	return nil
+}
+
+func (acme *acmeConfig) registerHTTPChallend(chal *challenge) error {
+	// TODO
+	return nil
+}
